ec2b0e0fafb15743b1d59118839674be61c853a9
Michael <michaelfernando@live.com>
2022-12-07 16:59:32 -0300

    correcao do erro

--- packages/docs/.storybook/main.js
+++ packages/docs/.storybook/main.js
@@ -17,7 +17,7 @@ module.exports = {
     "storyStoreV7": true
   }
 },
-viteFinal; (config, { configType}) => {
+viteFinal (config, { configType }) => {
     if (configType === 'PRODUCTION') {
         config.base = '/Storybook/'
 
